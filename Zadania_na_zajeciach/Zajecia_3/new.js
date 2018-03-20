10fec59e18eea7552c2ede6ed7b421979deca695
rom3k <romaszkin13@o2.pl>
2018-03-20 12:05:10 +0100

    Add broken js

---
+++ Zadania_na_zajeciach/Zajecia_3/new.js
@@ -0,0 +1,20 @@
+console.log("start");
+const anArray = new Array(3);
+anArray.push("0");
+console.log(anArray);
+
+const height = 10;
+const width = 12;
+console.log(width * height);
+
+let list = ['element 1', 'Element 2', 'Element 3'];
+console.log(list[0].toUpperCase());
+
+width.toFixed(30);
+
+const user = {
+    firstName: "Bartek",
+    lastName: "Nowak",
+    age: 25
+}
+console.log(user.age.toString());

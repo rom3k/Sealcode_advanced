01521047ecc44f6971107f92fae03b097d1d4117
rom3k <romaszkin13@o2.pl>
2018-03-31 15:21:55 +0200

    Backend homework update

--- Zadania_na_zajeciach/backend/Zajecia_1/my-app/index.js
+++ Zadania_na_zajeciach/backend/Zajecia_1/my-app/index.js
@@ -1,13 +1,21 @@
+
 var fs = require("fs");
 var d = new Date;
-var timeout = process.argv[2];
+var timeout = parseInt(process.argv[2], 10);
 var file = process.argv[3];
 
+if (isNaN(timeout)) {
+    console.log('Time is defined badly.');
+}
 
-setTimeout(() => {
-    fs.appendFile(file, '\n'+ d, (err) => {
-        if (err) throw err;
-    })    
-}, parseInt(timeout, 10));
-
+else if (!/([a-zA-Z0-9]+)(\.)([a-z]+)/.test(file)) {
+    console.log('File is defined badly.');
+}
+else {
+    setTimeout(() => {
+        fs.appendFile(file, '\n'+ d, (err) => {
+            if (err) throw err;
+        })    
+    }, timeout);
+}
 
